559df9a254b0e7c4e34b84b1ffe16cc52411c24f
VijayDeshwal339 <deshwalamit339@gmail.com>
2024-11-20 12:57:44 +0530

    done

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect('mongodb+srv://deshwalamit339:5Qy96YcYPQcjHtvX@cluster0.hwnlt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
